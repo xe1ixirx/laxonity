c501e3728dadbe25ed558a28d8a98f04ff863bf4
Your Name <e1ixir@proton.me>
2024-04-16 11:46:48 +0300

    Initial commit

---
+++ public/js/mailtype.js
@@ -0,0 +1,10 @@
+const mail = document.getElementById('mailtype');
+const test = document.getElementById('test');
+
+function showMail(){
+    const data = mail.value;
+
+
+    window.alert(data);
+}
+
